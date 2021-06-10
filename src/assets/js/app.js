71bfb143005f70ea3defdf94eae5bef3e4bdc6f2
Toxxify <Hellhammer220bpm@gmail.com>
2021-06-10 16:43:39 +0300

    layout completed

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,81 +1,110 @@
-// WINDOW WIDTH DETECT
-let currentWidth = $(window).width();
-$(window).on('resize', function () {
-    currentWidth = $(window).width();
-});
+// // WINDOW WIDTH DETECT
+// let currentWidth = $(window).width();
+// $(window).on('resize', function () {
+//     currentWidth = $(window).width();
+// });
 
-//App
-const Application = {
-    init: function() {
-        this.events();
-    },
-    events: function() {
-        !window.reinit && (window.reinit = {});
+// //App
+// const Application = {
+//     init: function() {
+//         this.events();
+//     },
+//     events: function() {
+//         !window.reinit && (window.reinit = {});
 
-        //main events
-        this.eventList.lazyImgs();
-        this.eventList.svgPolifill();
-        this.eventList.mask();
+//         //main events
+//         this.eventList.lazyImgs();
+//         this.eventList.svgPolifill();
+//         this.eventList.mask();
 
-        //app events
-        //this.eventList.sliders();
-    },
-    eventList: {
-        lazyImgs: function(){
+//         //app events
+//         //this.eventList.sliders();
+//     },
+//     eventList: {
+//         lazyImgs: function(){
 
-            let callback_loaded = function(el) {
-                const $img = $(el),
-                      $img_wrap = $img.closest('.lazy-img-wrap');
-                $img_wrap.addClass('loaded');
-            };
+//             let callback_loaded = function(el) {
+//                 const $img = $(el),
+//                       $img_wrap = $img.closest('.lazy-img-wrap');
+//                 $img_wrap.addClass('loaded');
+//             };
 
-            new LazyLoad({
-                elements_selector: ".lazy-img",
-                threshold: 0,
-                callback_loaded: callback_loaded,
-            });
-        },
-        svgPolifill: function () {
-            // svg sprites
-            svg4everybody && svg4everybody();
-        },
-        mask: function() {
-            $("[type=tel]").inputmask({
-                mask: '+7 (999) 99-99-99',
-                "clearIncomplete": false,
-                "showMaskOnHover": false,
-                "showMaskOnFocus": true,
-                "clearMaskOnLostFocus": true
-            });
-        },
+//             new LazyLoad({
+//                 elements_selector: ".lazy-img",
+//                 threshold: 0,
+//                 callback_loaded: callback_loaded,
+//             });
+//         },
+//         svgPolifill: function () {
+//             // svg sprites
+//             svg4everybody && svg4everybody();
+//         },
+//         mask: function() {
+//             $("[type=tel]").inputmask({
+//                 mask: '+7 (999) 99-99-99',
+//                 "clearIncomplete": false,
+//                 "showMaskOnHover": false,
+//                 "showMaskOnFocus": true,
+//                 "clearMaskOnLostFocus": true
+//             });
+//         },
 
-        sliders: function(){
-            $('.js-swiper-banner').each(function(i){
-                let bannerSwiper = new Swiper($('.js-swiper-banner .swiper-container')[i], {
-                    // Disable preloading of all images
-                    //preloadImages: false,
-                    // Enable lazy loading
-                    //lazy: {
-                    //    loadPrevNext: true,
-                    //},
-                    autoplay: {
-                        delay: 5000,
-                    },
-                    loop: true,
-                    spaceBetween: 15,
+//         sliders: function(){
+//             $('.js-swiper-banner').each(function(i){
+//                 let bannerSwiper = new Swiper($('.js-swiper-banner .swiper-container')[i], {
+//                     // Disable preloading of all images
+//                     //preloadImages: false,
+//                     // Enable lazy loading
+//                     //lazy: {
+//                     //    loadPrevNext: true,
+//                     //},
+//                     autoplay: {
+//                         delay: 5000,
+//                     },
+//                     loop: true,
+//                     spaceBetween: 15,
 
-                    // Navigation arrows
-                    navigation: {
-                        nextEl: $('.js-swiper-banner .swiper-button-next')[i],
-                        prevEl: $('.js-swiper-banner .swiper-button-prev')[i],
-                    },
-                });
-            });
+//                     // Navigation arrows
+//                     navigation: {
+//                         nextEl: $('.js-swiper-banner .swiper-button-next')[i],
+//                         prevEl: $('.js-swiper-banner .swiper-button-prev')[i],
+//                     },
+//                 });
+//             });
             
-        },
+//         },
+//     }
+// };
+// $(function () {
+//     'use strict';
+//     Application.init();
+// });
+
+
+const swiper = new Swiper('.big-slider', {
+    breakpoints: {
+    991: {
+        watchOverflow: true,
+        slidesPerView: 5,
+        spaceBetween: 35,
+        loop: false,
+    },
+    680: {
+        slidesPerView: 4,
+        spaceBetween: 30,
+    },
+    480: {
+        slidesPerView: 3,
+        spaceBetween: 25,
+    },
+    320: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+    },
+    20: {
+        slidesPerView: 1,
+        spaceBetween: 15,
+        loop: true,
     }
-};
-$(function () {
-    'use strict';
-    Application.init();
-});
+    }
+  });
